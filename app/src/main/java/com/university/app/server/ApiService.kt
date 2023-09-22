789241ff4bfe6d715378f3d46311c5d33c452e01
abhishek <abhishek.pandey@xcubelabs.com>
2023-09-22 21:12:01 +0530

    Added ont new layout file and change in apiservice

--- app/src/main/java/com/university/app/server/ApiService.kt
+++ app/src/main/java/com/university/app/server/ApiService.kt
@@ -1,10 +1,14 @@
 package com.university.app.server
 
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
     @GET("search?country=United+States")
     suspend fun getUniversityListFromServer()
+
+    @POST()
+    suspend fun getUserDetails()
 
 }
